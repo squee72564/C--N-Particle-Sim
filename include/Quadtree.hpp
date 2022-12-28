f8665a951ff5a12f2ef9193f93b5075ad7e3a805
Alexander Rodriguez <rodriguez.al183@gmail.com>
2022-12-27 18:13:35 -0800

    Added constructors, display and split function to quadtree.

--- include/Quadtree.hpp
+++ include/Quadtree.hpp
@@ -10,21 +10,34 @@
 class QuadTree {
 public:
   int m_level;
+
   float height;
   float width;
+  
   sf::RectangleShape m_rect;
+  
   sf::Vector2f origin;
+  
   std::array<QuadTree*, 4> m_subnode;
   std::vector<int> m_index;
+
   bool isLeaf;
 
-  //void split();
+  
   //bool contain() const;
   //bool contain_rect() const;
   //void set_color();
 
-  QuadTree(const int m_level, sf::Vector2f ori,  float h, float w);
-  void insert();
+  QuadTree();
+  QuadTree(const int m_level, sf::Vector2f ori, float h, float w);
+  QuadTree(const int m_level, float h, float w);
+  QuadTree(const QuadTree& qt);
+  
+  void split();
+  void display(sf::RenderWindow* gameWindow);
+  
+  //QuadTree(QuadTree&& qt);
+  //void insert();
 
   //void update();
   //void get() const;
--- src/Quadtree.cpp
+++ src/Quadtree.cpp
@@ -1,23 +1,103 @@
 #include "QuadTree.hpp"
 
-QuadTree::QuadTree(const int m_level, sf::Vector2f ori, float h, float w)
-  : m_level(m_level),
-    width(w),
-    height(h),
-    origin(ori),
+QuadTree::QuadTree()
+  : m_level(0),
+    width(1280),
+    height(800),
     m_subnode{nullptr, nullptr, nullptr, nullptr},
     m_index{NULL},
-    isLeaf(false)
+    isLeaf(true)
 {
   m_index.reserve(NODE_CAPACITY);
-  m_rect.setPosition(sf::Vector2f(ori.x-(w/2),ori.y-(h/2) ));
+  m_rect.setPosition(1280-width, 800-height);
   m_rect.setSize(sf::Vector2f(width,height));
   m_rect.setOutlineColor(sf::Color::Blue);
   m_rect.setOutlineThickness(1);
   m_rect.setFillColor(sf::Color::Transparent);
+  origin = sf::Vector2f(0,0);
 }
 
-QuadTree::insert()
+QuadTree::QuadTree(const int m_level, sf::Vector2f ori, float h, float w)
+  : m_level(m_level),
+    width(w),
+    height(h),
+    m_subnode{nullptr, nullptr, nullptr, nullptr},
+    m_index{NULL},
+    isLeaf(true)
 {
+  m_index.reserve(NODE_CAPACITY);
+  m_rect.setPosition(ori);
+  m_rect.setSize(sf::Vector2f(width,height));
+  m_rect.setOutlineColor(sf::Color::Blue);
+  m_rect.setOutlineThickness(1);
+  m_rect.setFillColor(sf::Color::Transparent);
+  origin = ori;
+}
+
+QuadTree::QuadTree(const int m_level, float h, float w)
+  : m_level(m_level),
+    width(w),
+    height(h),
+    m_subnode{nullptr, nullptr, nullptr, nullptr},
+    m_index{NULL},
+    isLeaf(true)
+{
+  m_index.reserve(NODE_CAPACITY);
+  m_rect.setPosition(1280-width, 800-height);
+  m_rect.setSize(sf::Vector2f(width,height));
+  m_rect.setOutlineColor(sf::Color::Blue);
+  m_rect.setOutlineThickness(1);
+  m_rect.setFillColor(sf::Color::Transparent);
+  origin = sf::Vector2f(0,0);
+}
+
+QuadTree::QuadTree(const QuadTree& qt)
+  : m_level(qt.m_level),
+    width(qt.width),
+    height(qt.height),
+    m_subnode{qt.m_subnode},
+    m_index{qt.m_index},
+    isLeaf(qt.isLeaf)
+{
+  m_index.reserve(NODE_CAPACITY);
+  m_rect.setPosition(qt.m_rect.getPosition());
+  m_rect.setSize(sf::Vector2f(width,height));
+  m_rect.setOutlineColor(sf::Color::Blue);
+  m_rect.setOutlineThickness(1);
+  m_rect.setFillColor(sf::Color::Transparent);
+  origin = sf::Vector2f(0,0);
+}
+
+void QuadTree::split()
+{
+  isLeaf = false;
+  m_subnode[0] = new QuadTree(m_level + 1, sf::Vector2f(origin.x, origin.y), height/2, width/2);
+  m_subnode[1] = new QuadTree(m_level + 1, sf::Vector2f(origin.x+ width/2, origin.y), height/2, width/2);
+  m_subnode[2] = new QuadTree(m_level + 1, sf::Vector2f(origin.x, origin.y + height/2), height/2, width/2);
+  m_subnode[3] = new QuadTree(m_level + 1, sf::Vector2f(origin.x + width/2, origin.y + height/2), height/2, width/2);
   
 }
+
+void QuadTree::display(sf::RenderWindow* gameWindow)
+{
+  if (isLeaf) {
+    gameWindow->draw(m_rect);
+    return;
+  }
+  
+  m_subnode[0]->display(gameWindow);
+  m_subnode[1]->display(gameWindow);
+  m_subnode[2]->display(gameWindow);
+  m_subnode[3]->display(gameWindow);
+}
+
+
+// QuadTree::QuadTree(QuadTree&& qt)
+// {
+  
+// }
+
+// void QuadTree::insert()
+// {
+  
+// }
