36988c43691e32ab8c68f3ce388a8c3e687a7d4d
squee72564 <rodriguez.al183@gmail.com>
2022-12-28 21:14:08 -0800

    Added copy and default constructor, and implemented id integer into constructors so particles have a unique id.
    Started the function for check collisions, it will need revising.
    
    TODO: Implement collision and gravity functionality to include QuadTree class

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -1,11 +1,38 @@
 #include "Particle.hpp"
 
+Particle::Particle()
+    : position(sf::Vector2f(0,0)),
+      velocity(sf::Vector2f(0,0)),
+      mass(1),
+      id(++PARTICLE_ID % 2000)
+{
+    radius = 1;
+    shape.setRadius(radius); // Set the radius of the circle to the mass of the particle
+    shape.setFillColor(sf::Color(255, 255, 255)); //white
+    shape.setOrigin(mass, mass); // Set the origin of the circle to its center
+}
+
 Particle::Particle(sf::Vector2f pos, sf::Vector2f vel, float m, std::mt19937& gen, std::uniform_int_distribution<>& dis)
-    : position(pos), velocity(vel), mass(m)
+    : position(pos),
+      velocity(vel),
+      mass(m),
+      id(++PARTICLE_ID % 2000)
 {
     radius = m;
     shape.setRadius(radius); // Set the radius of the circle to the mass of the particle
     shape.setFillColor(sf::Color(dis(gen), dis(gen), dis(gen))); //multicolored
+    shape.setOrigin(mass, mass); // Set the origin of the circle to its center
+}
+
+Particle::Particle(const Particle& particle)
+    : position(particle.position),
+      velocity(particle.velocity),
+      mass(particle.mass),
+      id(particle.id)
+{
+    radius = mass;
+    shape.setRadius(radius); // Set the radius of the circle to the mass of the particle
+    shape.setFillColor(particle.shape.getFillColor()); //multicolored
     shape.setOrigin(mass, mass); // Set the origin of the circle to its center
 }
 
@@ -52,6 +79,41 @@ void Particle::update(float dt, std::list<Particle>& particles)
 
     position += velocity * dt;
 }
+
+void Particle::checkCollision(std::list<Particle>& particles)
+{
+    for (auto& other : particles)
+    {
+        if(this == &other)
+        {
+            continue;
+        }
+        
+        // Calculate the distance squared between the two particles as a scalar.
+        float distanceSquared = dot(position - other.position, position - other.position);
+
+        // If the distance is smaller than the sum of the radii, the particles are colliding
+        if (distanceSquared != 0 && distanceSquared <= (radius + other.radius) * (radius + other.radius))
+        {
+            // Handle collision here
+            // Normalized point of contact vector
+            sf::Vector2f rHat = (other.position - position) * inv_Sqrt(distanceSquared);
+
+            // Component for current particle velocity along rHat direction
+            float a1 = dot(velocity, rHat);
+                // Component for other particle velocity along rHat direction
+            float a2 = dot(other.velocity, rHat);
+            
+            // Momentum change due to collision
+            float p = 2 * mass * other.mass * (a1-a2)/(mass + other.mass);
+
+            velocity -= p/mass * (rHat);
+            other.velocity += p/other.mass * (rHat); 
+        }
+    }
+
+}
+
 
 template <typename T>
 float dot(const sf::Vector2<T>& vec1, const sf::Vector2<T>& vec2)
