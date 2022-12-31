814996c30e3b7796fd13d0c0409b5d6ff1494b22
squee72564 <rodriguez.al183@gmail.com>
2022-12-30 19:54:56 -0800

    Removed Particle::update() as it will not be a part of the QuadTree class

--- include/Particle.hpp
+++ include/Particle.hpp
@@ -21,15 +21,6 @@ public:
     Particle(sf::Vector2f pos, sf::Vector2f vel, float m, std::mt19937& gen, std::uniform_int_distribution<>& dis);
     Particle(const Particle& particle);
     ~Particle();
-
-    void update(float dt, std::list<Particle>& particles);
-    void checkCollision(std::list<Particle>& particles);
 };
-
-
-template <typename T>
-inline float dot(const sf::Vector2<T>& vec1, const sf::Vector2<T>& vec2);
-
-inline float inv_Sqrt(float number);
 
 #endif
