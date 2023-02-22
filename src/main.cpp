5145efa5282aa96c1782f97350d3c1a9cf853435
Alexander Rodriguez <rodriguez.al183@gmail.com>
2023-02-22 06:18:29 -0800

    changed linking command for future reference

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-//g++ .\main.o .\Particle.o .\ParticleSimulation.o -o main -L"C:\SFML-2.5.1\lib" -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
+//g++ .\main.o .\Particle.o .\ParticleSimulation.o .\Quadtree.o -o main -L"C:\SFML-2.5.1\lib" -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 //g++ -c -Wall .\src\main.cpp .\src\Particle.cpp .\src\ParticleSimulation.cpp -I"C:\SFML-2.5.1\include" -I"C:\SFML-2.5.1\bin" -I".\include\"
 
 #include "ParticleSimulation.hpp"
