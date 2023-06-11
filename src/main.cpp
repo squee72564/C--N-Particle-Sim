9e22a4b8245ba1c60d80c9d210068a4f7dce4501
Alexander Rodriguez <rodriguez.al183@gmail.com>
2023-06-10 20:06:36 -0700

    Added number of threads to particleSim constructor

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main()
     sf::Vector2f grav = sf::Vector2f(0,0);
 
     //Start Particle Simulation
-    ParticleSimulation particleSimulation(TIME_STEP, grav, window);
+    ParticleSimulation particleSimulation(TIME_STEP, grav, window, 6);
     particleSimulation.run();
 
     return 0;
