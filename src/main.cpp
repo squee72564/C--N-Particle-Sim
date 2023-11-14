c5fb374e019f0fd4f5991cdc70848b97e33733a5
squee72564 <rodriguez.al183@gmail.com>
2023-11-14 10:40:24 -0800

    Edited profiling method for multi threading

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-//g++ -c -Wall .\src\main.cpp .\src\Particle.cpp .\src\ParticleSimulation.cpp .\src\QuadTree.cpp -I"C:\SFML-2.5.1\include" -I"C:\SFML-2.5.1\bin" -I".\include\"
-//g++ .\main.o .\Particle.o .\ParticleSimulation.o .\Quadtree.o -o main -L"C:\SFML-2.5.1\lib" -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
+// g++ -c -Wall .\src\main.cpp .\src\Particle.cpp .\src\ParticleSimulation.cpp .\src\QuadTree.cpp -I"C:\SFML-2.5.1\include" -I"C:\SFML-2.5.1\bin" -I".\include\"
+// g++ .\main.o .\Particle.o .\ParticleSimulation.o .\Quadtree.o -o main -L"C:\SFML-2.5.1\lib" -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 
 #include "ParticleSimulation.hpp"
 
@@ -12,8 +12,8 @@ const int WINDOW_HEIGHT = 1080;
 
 int main()
 {
-    //Gravity for particles
-    sf::Vector2f grav = sf::Vector2f(0,0);
+    // Gravity for particles
+    sf::Vector2f grav = sf::Vector2f(0, 0);
 
     // Get the current time
     long long timeInSeconds = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
@@ -27,17 +27,28 @@ int main()
 
     outputFile.close();
 
-    for (int i = 8; i <= 15; i++) { // threads
-        for (int j = 3; j <= 5; j++) { // tree depth
-            for (int k = 3; k <= 5; k++) { // node cap
-                // Create the window
-                sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Particle Simulator");
-                window.setFramerateLimit(60); // Limit the frame rate to 60 FPS
+    for (int i = 8; i <= 15; i++) // threads
+    {
+        for (int j = 5; j <= 7; j++) // tree depth
+        {
+            // Create the window
+            sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Particle Simulator");
+            window.setFramerateLimit(60); // Limit the frame rate to 60 FPS
 
-                //Start Particle Simulation
-                ParticleSimulation particleSimulation(TIME_STEP, grav, window, i, j, k, logName);
-                particleSimulation.run();
+            // get dimension of smallest node depth
+            int w = WINDOW_WIDTH;
+            int h = WINDOW_HEIGHT;
+            for (int k = 0; k < j; k++)
+            {
+                w /= 2;
+                h /= 2;
             }
+
+            int nodeCap = w * h;
+
+            // Start Particle Simulation
+            ParticleSimulation particleSimulation(TIME_STEP, grav, window, i, j, nodeCap, logName);
+            particleSimulation.run();
         }
     }
 
