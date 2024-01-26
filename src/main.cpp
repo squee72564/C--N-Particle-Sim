c10cec8594071355c92a8b59a8534eac1231a58e
squee72564 <rodriguez.al183@gmail.com>
2024-01-26 10:18:47 -0800

    Changed default threads to 6, increased max depth to 6, and increased node cap to 24

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,3 @@
-//g++ -c -Wall .\src\main.cpp .\src\Particle.cpp .\src\ParticleSimulation.cpp .\src\QuadTree.cpp -I"C:\SFML-2.5.1\include" -I"C:\SFML-2.5.1\bin" -I".\include\"
-//g++ .\main.o .\Particle.o .\ParticleSimulation.o .\Quadtree.o -o main -L"C:\SFML-2.5.1\lib" -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
-
 #include "ParticleSimulation.hpp"
 
 // Delta Time
@@ -20,7 +17,7 @@ int main()
     window.setFramerateLimit(60); // Limit the frame rate to 60 FPS
 
     //Start Particle Simulation
-    ParticleSimulation particleSimulation(TIME_STEP, grav, window, 8, 3, 4);
+    ParticleSimulation particleSimulation(TIME_STEP, grav, window, 12, 6, 24);
     particleSimulation.run();
 
     return 0;
