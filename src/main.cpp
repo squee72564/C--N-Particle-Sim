6af35985a9e4f6fffd9b04e3ee8ed848229f0072
Alexander Rodriguez <rodriguez.al183@gmail.com>
2024-01-29 08:58:28 -0800

    Edited starting parameters for sim

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ int main()
     window.setFramerateLimit(60); // Limit the frame rate to 60 FPS
 
     //Start Particle Simulation
-    ParticleSimulation particleSimulation(TIME_STEP, grav, window, 10, 5, 24);
+    ParticleSimulation particleSimulation(TIME_STEP, grav, window, 10, 7, 256);
     particleSimulation.run();
 
     return 0;
