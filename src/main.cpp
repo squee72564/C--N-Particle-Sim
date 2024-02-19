a259444ccf366dfcbeed78b69131dbb822b33fc4
squee72564 <rodriguez.al183@gmail.com>
2024-02-19 07:28:43 -0800

    Added ability to input simulation width and height as command line argument

--- src/main.cpp
+++ src/main.cpp
@@ -16,17 +16,22 @@ int main(int argc, char* argv[])
     int num_threads = 1;
     int max_depth = 8;
     int cap = 64;
+    int simulation_width = WINDOW_WIDTH;
+    int simulation_height = WINDOW_HEIGHT;
 
-    if (argc < 4) {
-        std::cout << "Usage: " << argv[0] << " <num threads> <tree max depth> <tree node capacity>\n";
+    if (argc < 6) {
+        std::cout << "Usage: " << argv[0] << " <num threads> <tree max depth> <tree node capacity> <sim width> <sim height>\n";
         return 1;
     } else {
         num_threads = std::atoi(argv[1]);
         max_depth = std::atoi(argv[2]);
         cap = std::atoi(argv[3]);
+        simulation_width = std::atoi(argv[4]);
+        simulation_height = std::atoi(argv[5]);
 
-        if (!num_threads || !max_depth || !cap) {
-            std::cout << "Usage: " << argv[0] << " <num threads> <tree max depth> <tree node capacity>\n";
+
+        if (!num_threads || !max_depth || !cap || !simulation_width || !simulation_height) {
+        std::cout << "Usage: " << argv[0] << " <num threads> <tree max depth> <tree node capacity> <sim width> <sim height>\n";
             std::cout << "--  Please ensure valid integers are passed as arguments.\n";
             return 1;
         }
@@ -37,7 +42,15 @@ int main(int argc, char* argv[])
     window.setFramerateLimit(60); // Limit the frame rate to 60 FPS
 
     //Start Particle Simulation 
-    ParticleSimulation particleSimulation(window, num_threads, TIME_STEP, grav, max_depth, cap);
+    ParticleSimulation particleSimulation(simulation_width,
+                                          simulation_height,
+                                          window,
+                                          num_threads,
+                                          TIME_STEP,
+                                          grav,
+                                          max_depth,
+                                          cap);
+
     std::cout << "Starting particle sim...\n";
     particleSimulation.run();
     std::cout << "Particle sim ended\n";
